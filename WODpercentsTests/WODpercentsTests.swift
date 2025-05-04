913a1942350f82f6f27de97142aa373560d34fe9
Sophareth Hao <67264049+HaoSophareth@users.noreply.github.com>
2025-05-04 01:01:09 -0500

    Added prework files to Repository

---
+++ WODpercentsTests/WODpercentsTests.swift
@@ -0,0 +1,17 @@
+//
+//  WODpercentsTests.swift
+//  WODpercentsTests
+//
+//  Created by HaoSophareth on 2/5/25.
+//
+
+import Testing
+@testable import WODpercents
+
+struct WODpercentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
